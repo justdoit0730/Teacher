98a9ca2c2c1691cc25c76aa212767289149917d7
justdo_IT <99151827+justdoit0730@users.noreply.github.com>
2022-11-19 11:05:26 +0900

    Delete src directory

--- src/main/java/com/tonari/config/ChatConstants.java
+++
@@ -1,9 +0,0 @@
-package com.tonari.config;
-
-public final class ChatConstants {
-
-	public final static String CHAT_EXCHANGE_NAME = "chat.exchange";
-	public final static String CHAT_QUEUE_NAME = "chat.user.";
-	public final static String ROUTING_KEY = "user.";
-
-}
